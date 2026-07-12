56d0b8228240d7d003c44b79f283e1c646985f3e
Siddarth Saha <siddarth@Siddarths-MacBook-Pro.local>
2026-07-13 00:38:50 +0530

    sync: 2026-07-13T00:38:50+05:30

--- doom/config.el
+++ doom/config.el
@@ -403,11 +403,21 @@
         ("h" "On hold" todo "HOLD")
         ("i" "Ideas" todo "IDEA")))
 (after! org
-  (setq org-capture-templates
-        '(("p" "Personal TODO" entry
-           (file "~/org/personal/life.org")
-           "* TODO %?\n  %U\n")
+  (dolist
+      (template
+       '(("P" "Personal")
+         ("Pt" "Personal task" entry
+          (file "~/org/personal/inbox.org")
+          "* TODO %?\n  %U\n")
+         ("Pn" "Personal note" entry
+          (file "~/org/personal/inbox.org")
+          "* %?\n  %U\n")
 
-          ("w" "Work TODO" entry
-           (file "~/org/work/index.org")
-           "* TODO %?\n  %U\n"))))
+         ("W" "Work")
+         ("Wt" "Work task" entry
+          (file "~/org/work/inbox.org")
+          "* TODO %?\n  %U\n")
+         ("Wn" "Work note" entry
+          (file "~/org/work/inbox.org")
+          "* %?\n  %U\n")))
+    (add-to-list 'org-capture-templates template t)))

0a192f66e928ce422276a0b3228044c66f651060
Siddarth Saha <siddarth@Siddarths-MacBook-Pro.local>
2026-07-13 02:57:02 +0530

    sync: 2026-07-13T02:57:02+05:30

--- doom/config.el
+++ doom/config.el
@@ -349,7 +349,7 @@
    consult-fd
    siddarth/project-file-search
    +default/search-project
-   :preview-key 'any))
+   :preview-key 'any)
 
   (defun siddarth/xref-show-definitions (fetcher alist)
     (let ((xrefs (funcall fetcher)))

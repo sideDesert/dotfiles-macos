159a6415beea259781c517c1c189c58ffbaf27e3
Siddarth Saha <siddarth@Siddarths-MacBook-Pro.local>
2026-08-28 15:37:08 +0530

    sync: 2026-08-28T15:37:08+05:30

--- doom/init.el
+++ doom/init.el
@@ -93,7 +93,7 @@
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
-       (debugger +lsp)          ; stepping through code, to help you add bugs
+       ;;(debugger +lsp)          ; stepping through code, to help you add bugs
        ;;direnv
        docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
